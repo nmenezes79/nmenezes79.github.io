213bd3ff9ca016928479290080232909cb573dee
Neil Menezes <iamnmenezes@gmail.com>
2025-09-10 05:20:03 +0530

    New Project

---
+++ lucky-draw-fetcher/script.js
@@ -0,0 +1,11 @@
+document.getElementById("drawBtn").addEventListener("click", function () {
+	fetch('https://randomuser.me/api/')
+		.then(response => response.json())
+		.then(data => {
+			let user = data.results[0];
+			document.getElementById("winnerImg").src = user.picture.large;
+			document.getElementById("winnerName").textContent = `Name: ${user.name.first} ${user.name.last}`;
+			document.getElementById("winnerEmail").textContent = `Email: ${user.email}`;
+		})
+		.catch(error => console.error("Error Fetching Data:", error));
+});
